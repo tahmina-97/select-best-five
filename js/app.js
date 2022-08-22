4c09aaf0545f43bd954a6973e5339f408043b3e2
Tahmina Ria <tahminariya459@gmail.com>
2022-08-22 17:49:41 +0600

    validation

--- js/app.js
+++ js/app.js
@@ -31,16 +31,33 @@ function selectToAdd(element){
 
 document.getElementById('btn-calculate').addEventListener('click', function(){
     const perPlayerCost = getFieldvalueById('cost-per-player');
-    const playerNumber = document.getElementById('list-items').childElementCount;
-    const playerExpense = perPlayerCost * playerNumber;
-    setElementById('player-expense', playerExpense);
+    if(perPlayerCost === 'failed'){
+        return;
+    }
+    else{
+        const playerNumber = document.getElementById('list-items').childElementCount;
+        const playerExpense = perPlayerCost * playerNumber;
+        setElementById('player-expense', playerExpense);
+
+    }
+    
 });
 
 document.getElementById('btn-calculate-total').addEventListener('click', function(){
     const playerExpenses = getElementValueById('player-expense');
     const managerCost = getFieldvalueById('cost-manager');
     const coachCost = getFieldvalueById('cost-coach');
-    const totalExpense = playerExpenses + managerCost + coachCost ;
-    setElementById('total-expense', totalExpense);
+    if(managerCost === 'failed' || coachCost === 'failed'){
+        return;
+    }
+    else if(playerExpenses === 'failed'){
+        return;
+    }
+    else{
+        const totalExpense = playerExpenses + managerCost + coachCost ;
+        setElementById('total-expense', totalExpense);
+
+    }
+    
 
 })
--- js/utilities.js
+++ js/utilities.js
@@ -1,11 +1,29 @@
 function getFieldvalueById(inputId){
     const getInputField = document.getElementById(inputId);
-    const getInputFieldValue = parseFloat(getInputField.value);
-    return getInputFieldValue;
+    const fieldValueStr = getInputField.value;
+    const fieldValue = parseFloat(fieldValueStr);
+    if(isNaN(fieldValueStr)){
+        alert('Input cannot be other than number');
+        return 'failed';
+       }
+       else if(fieldValueStr.length === 0){
+        alert('Input Field cannot remain empty');
+        return 'failed';
+       }
+       else if(fieldValue < 0){
+        alert('Input value cannot be a negative number');
+        return 'failed';
+       }
+    return fieldValue;
 }
 function getElementValueById(elementId){
     const getElement = document.getElementById(elementId);
-    const getElementValue = parseFloat(getElement.innerText);
+    const getElementStr = getElement.innerText;
+    const getElementValue = parseFloat(getElementStr);
+    if(getElementStr.length === 0){
+        alert('Yoy have to calculate the Player Expense first');
+        return 'failed';
+    }
     return getElementValue;
 }
 
